f3901e7debb8e2affea3f6fc015a52dd6d324f2a
zakpdev <135481838+zakpdev@users.noreply.github.com>
2023-08-31 23:57:03 -0300

    Add button to allow users to set grid size

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 //grab the grid-container div
 const gridContainer = document.querySelector(".grid-container");
 
+/* Launch a prompt for the user's desired grid size when the button is clicked 
+and convert the return to an integer. Note: Input validation is out of scrope */
+/* const sizeButton = querySelector("#size-button");
+
+sizeButton.addEventListener("click", prompt("Enter the number of squares per side (Max 100)", "Please Enter a Whole Number Between 1 and 100"));
+
+ */
+
+
 //function to create the grid. Creates the chosen number of rows first and then fills each with cells
 function createGrid() {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 64; i++) {
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row');
     gridContainer.appendChild(rowDiv);
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < 64; j++) {
       const gridRow = gridContainer.children[i];
       const cellDiv = document.createElement('div');
       cellDiv.classList.add('column');
 
-      //event listener to "color in" the cell when the mouse hovers over it
+      //adds event listener to "color in" the cell when the mouse hovers over it
       cellDiv.addEventListener("mouseover", function() {
         cellDiv.style.backgroundColor = "black";        
       });
