798014113da2cdec4b0de4fdf89c8f7800c47912
zakpdev <135481838+zakpdev@users.noreply.github.com>
2023-09-10 16:31:01 -0300

    Add conditional to set default grid size and store user's choice between page refreshes

--- main2.js
+++ main2.js
@@ -1,11 +1,44 @@
-//Define function to prompt user to set grid size and link it to the Set Size button
-let userSize;
-let gridSize = 16; //default value
+
+let gridSize;
+
+gridSize = sessionStorage.getItem("userSelection");
+
+if (gridSize == "" || gridSize == null) {
+  gridSize = 64; //default value
+} else {
+  gridSize = parseInt(gridSize);
+}
 
 function setSize() {
-  userSize = parseInt(prompt("Enter your desired grid size", "Enter a whole number between 1 - 100"));
+  gridSize = prompt("Enter your desired grid size as a whole number between 1 - 100");
+  sessionStorage.setItem("userSelection", gridSize);
+  location.reload();
 }
 
 const sizeButton = document.querySelector(".size-button");
 
 sizeButton.addEventListener('click', setSize);
+
+const gridContainer = document.querySelector(".grid");
+
+function createGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+    gridContainer.appendChild(rowDiv);
+    for (let j = 0; j < gridSize; j++) {
+      const gridRow = gridContainer.children[i];
+      const cellDiv = document.createElement('div');
+      cellDiv.classList.add('column');
+
+      //adds event listener to "color in" the cell when the mouse hovers over it
+      cellDiv.addEventListener("mouseover", function() {
+        cellDiv.style.backgroundColor = "black";        
+      });
+
+      gridRow.appendChild(cellDiv);
+    }  
+  }
+}
+
+createGrid();
